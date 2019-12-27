a1d5640bee3430c63b26c82246ffe51188d70eeb
nedialkom <nedialkom@gmail.com>
2019-12-27 21:36:25 +0200

    collectstatic --noinput

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 exec gunicorn -c gconfig.py --log-file=- my_openshift.wsgi:application
