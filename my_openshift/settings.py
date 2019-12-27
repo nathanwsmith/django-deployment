9e753e8f5e91a34675a0c96c2a9ae31202102e4d
nedialkom <nedialkom@gmail.com>
2019-12-27 20:48:37 +0200

    cfrc cookie domain

--- my_openshift/settings.py
+++ my_openshift/settings.py
@@ -123,4 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CSRF_COOKIE_SECURE=False
+#CSRF_COOKIE_SECURE=False
+CSRF_COOKIE_DOMAIN = '127.0.0.1'
