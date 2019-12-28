a56a011e4f424d9473b8cd5833f6c493877bd31e
nedialkom <nedialkom@gmail.com>
2019-12-29 01:25:34 +0200

    VERSION 1.4 - working static files

--- main/views.py
+++ main/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, redirect
 
 def main(request):
     if request.user.is_authenticated:
-        return render(request, 'mainPage.html')
+        return mainPage(request)
     else:
         return render(request, 'welcome.html')
+
+@login_required
+def mainPage(request):
+    return render(request, 'mainPage.html')
