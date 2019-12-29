e94029fe0a9d51aa6b363c77017cb9ada16982c4
nedialkom <nedialkom@gmail.com>
2019-12-29 23:19:07 +0200

    VERSION 1.6 - sign in template for new user and redirect to login for existing with pre-filled e-mail

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('signup/', views.signup, name='signup'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,36 +1,45 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.http import HttpResponseRedirect
-from django.http import HttpResponse
-from django.urls import reverse
 
 def welcome(request):
-    if request.user.is_authenticated:
+    if request.user.is_authenticated: #loged in user
         return mainPage(request)
-    else:
-        return render(request, 'welcome.html')
+    else: # not logged in user
+        if request.method == 'POST': #a attempt for sign-up
+            email = request.POST.get('email', None)
+            return signup(request, email)
+        else: # GET request
+            if request.GET.get('email') is not None:
+                return signup(request, email=request.GET.get('email', None))
+            else:
+                return render(request, 'welcome.html')
 
 @login_required
 def mainPage(request):
     return render(request, 'mainPage.html')
 
-# not working def as it is stopped from urls.py
 def login(request):
-    if request.method == 'POST':
-        # Sign in button is pushed
-        if not request.POST.get('remember_me', None):
-            request.session.set_expiry(0)
+    if request.method == 'POST': # Sign in button is pushed
+        if not request.POST.get('remember_me', None): request.session.set_expiry(0)
         user = authenticate(username=request.POST.get('username', None), password=request.POST.get('password', None))
-        if user is None:
-            # Wrong credentials
+        if user is None: # Wrong credentials
             context = {'error': 'Wrong credentials!'}
-        else:
-            # Correct credentials
+        else: # Correct credentials
             context = None
         return auth_views.LoginView.as_view(template_name='login.html', extra_context = context)(request)
-    else:
-        # Get request -> load the page
+    else: # Get request -> load the page
         return render(request, 'login.html')
+
+def signup(request, email=None):
+    try:
+        user = User.objects.get(username=email)
+    except User.DoesNotExist:
+        user = None
+    if user is None:
+        context = {'email':'New user {}'.format(email)}
+        return render(request, 'signup.html', context=context)
+    else:
+        return render(request, 'login.html', {'email': email})
