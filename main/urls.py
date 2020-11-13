f62a35de7ab8ff7aca87cae48fbfd956920783d0
nathanwsmith <nathan.smith11@baesystems.com>
2020-11-13 13:48:40 +0000

    removed urls.py as our code hasn't set these yet

--- main/urls.py
+++ main/urls.py
@@ -1,16 +1,24 @@
-from django.urls import path
+"""adsb URL Configuration
 
-from . import views
-
-from django.contrib.auth import views as auth_views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),
-    path('login/', views.login, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('signup/', views.signup, name='signup'),
-    path('accounts/', views.accounts, name='accounts'),
-    path('accounts/new/', views.account_new, name="account_new"),
-    path('accounts/<int:pk>/edit/', views.account_edit, name='account_edit'),
-    path('accounts/<int:pk>/delete/', views.account_delete, name='account_delete'),
-]
+    path('', include('main.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
